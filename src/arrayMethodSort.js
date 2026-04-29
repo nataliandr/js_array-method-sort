9e7ac52b61cd61d097fa3f53d3ec011b0ffc805c
nataliandr <fnnny.yann@mail.com>
2026-04-29 14:35:18 +0200

    add arrayMethodSort Solution

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -4,8 +4,42 @@
  * Implement method Sort
  */
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const defaultCompare = (a, b) => {
+      const sA = String(a);
+      const sB = String(b);
+
+      if (sA < sB) {
+        return -1;
+      }
+
+      if (sA > sB) {
+        return 1;
+      }
+
+      return 0;
+    };
+
+    const compare = compareFunction || defaultCompare;
+
+    // 2. Алгоритм Bubble Sort
+    // Робимо копію довжини, щоб не звертатися до this.length постійно
+    const len = this.length;
+
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len - 1 - i; j++) {
+        // Викликаємо compare з поточними елементами
+        if (compare(this[j], this[j + 1]) > 0) {
+          // Міняємо місцями (Swap)
+          const temp = this[j];
+
+          this[j] = this[j + 1];
+          this[j + 1] = temp;
+        }
+      }
+    }
+
+    return this;
   };
 }
 
